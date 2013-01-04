fa78663fb85130856e9cd666a9f5078d1ac76213
Chris Smith <github.com@chris.cs278.org>
2013-01-04 01:39:15 -0800

    Add jQuery bindings

---
+++ src/jquery.mailcheck.js
@@ -0,0 +1,42 @@
+/*
+ * Mailcheck https://github.com/Kicksend/mailcheck
+ * Author
+ * Derrick Ko (@derrickko)
+ *
+ * License
+ * Copyright (c) 2012 Receivd, Inc.
+ *
+ * Licensed under the MIT License.
+ *
+ * v 1.1
+ */
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'mailcheck'], factory);
+  } else {
+    if (root.jQuery) {
+      factory(root.jQuery, root.Kicksend.mailcheck);
+    }
+  }
+}(this, function($, mailcheck) {
+  $.fn.mailcheck = function(opts) {
+    var self = this;
+    if (opts.suggested) {
+      var oldSuggested = opts.suggested;
+      opts.suggested = function(result) {
+        oldSuggested(self, result);
+      };
+    }
+
+    if (opts.empty) {
+      var oldEmpty = opts.empty;
+      opts.empty = function() {
+        oldEmpty.call(null, self);
+      };
+    }
+
+    opts.email = this.val();
+    mailcheck.run(opts);
+  }
+}));
